1fb9c9ff4267419a457cf6cfd91f559cf124e5ca
Pierre Aillet <pierreaillet@MacBook-Air-de-Pierre.local>
2023-09-09 00:33:47 +0200

    [ADD]: add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Utilisez CORS comme middleware
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
@@ -26,14 +26,16 @@ app.post('/upload', upload.single('image'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: 'Aucun fichier téléchargé.' });
     }
-
-    const publicLink = `http://localhost:${port}/uploads/${req.file.filename}`;
+    const publicLink = `https://api-test-ashen.vercel.app/uploads/${req.file.filename}`;
     console.log(publicLink)
     res.json({ message: 'Image téléchargée avec succès', link: publicLink });
 });
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.get('/', (req, res) => {
+    res.send("MY API")
+})
 
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
