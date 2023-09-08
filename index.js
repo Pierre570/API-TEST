0c8817fc15e64246fff95e5b7e71984944672faa
Pierre Aillet <pierreaillet@MacBook-Air-de-Pierre.local>
2023-09-09 00:21:57 +0200

    [ADD]: api

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const multer = require('multer');
+const path = require('path'); // Importez le module path
+const cors = require('cors'); // Importez le module CORS
+
+// Configuration de Multer pour le téléchargement des fichiers
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+// Utilisez CORS comme middleware
+app.use(cors());
+
+app.use(express.json());
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'Aucun fichier téléchargé.' });
+    }
+
+    const publicLink = `http://localhost:${port}/uploads/${req.file.filename}`;
+    console.log(publicLink)
+    res.json({ message: 'Image téléchargée avec succès', link: publicLink });
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
